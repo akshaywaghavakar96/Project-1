cb3be10b5d571c23ca1b093b4594ddc83b696092
Akshay Waghavakar <56022915+akshaywaghavakar96@users.noreply.github.com>
2026-06-13 21:53:35 +0530

    Add transaction checks for success and failed rates
    Added checks for total transactions before calculating success and failed rates.

--- app.py
+++ app.py
@@ -34,41 +34,26 @@ filtered_df = df[
     (df["status"].isin(selected_status))
 ]
 
-
 total_txns = len(filtered_df)
 
 total_amount = filtered_df["amount"].sum()
 
-success_rate = round(
-    len(filtered_df[filtered_df["status"]=="Success"])
-    / total_txns * 100,
-    2
-)
+if total_txns > 0:
+    success_rate = round(
+        (
+            len(filtered_df[filtered_df["status"] == "Success"])
+            / total_txns
+        ) * 100,
+        2,
+    )
 
-failed_rate = round(
-    len(filtered_df[filtered_df["status"]=="Failed"])
-    / total_txns * 100,
-    2
-)
-
-col1,col2,col3,col4 = st.columns(4)
-
-col1.metric(
-    "Total Transactions",
-    f"{total_txns:,}"
-)
-
-col2.metric(
-    "Transaction Amount",
-    f"₹{total_amount:,.0f}"
-)
-
-col3.metric(
-    "Success %",
-    f"{success_rate}%"
-)
-
-col4.metric(
-    "Failed %",
-    f"{failed_rate}%"
-)
+    failed_rate = round(
+        (
+            len(filtered_df[filtered_df["status"] == "Failed"])
+            / total_txns
+        ) * 100,
+        2,
+    )
+else:
+    success_rate = 0
+    failed_rate = 0
